77dce5575450c5cecda0089af9166766e9a272c5
catruge <49883288+catruge@users.noreply.github.com>
2021-02-25 14:45:56 -0500

    Rename rotate to rotateCamera to avoid conflict with built-in math.js rotate method

--- src/index.js
+++ src/index.js
@@ -631,7 +631,7 @@ math.import({
 
         return m;
     },
-    rotate: function(rx, ry, rz) { // rotates the camera
+    rotateCamera: function(rx, ry, rz) { // rotates the camera
         let rxyz = [rx, ry, rz];
         if (!isNaN(math.sum(rxyz))) {
             cam.properties[frame].rxyz = rxyz;
